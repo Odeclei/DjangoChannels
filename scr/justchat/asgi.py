33a660da2b023344434c3cb4de5bbd9df0eaf293
Odeclei <odeclei_tamanini@hotmail.com>
2024-05-13 17:02:10 -0300

    https://channels.readthedocs.io/en/latest/tutorial/part_2.html

--- scr/justchat/asgi.py
+++ scr/justchat/asgi.py
@@ -1,3 +1,5 @@
+# flake8: noqa
+# type: ignore
 """
 ASGI config for justchat project.
 
@@ -9,14 +11,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justchat.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
-  {
-    "http": get_asgi_application(),
-    # Just HTTP for now. (we cand add other protocols later.)
-  }
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
 )
